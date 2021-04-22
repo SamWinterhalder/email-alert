c8cc80611a45dfc895e2b4f8182091f7d9bd1f0e
Sam <winterhalder.sam@gmail.com>
2021-04-22 23:28:35 +0100

    Correct indentation and move print

--- find_email.py
+++ find_email.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+# /usr/bin/python3
 
 import email
 import os
@@ -88,6 +88,5 @@ if __name__ == "__main__":
                     "announcement": os.environ.get("ANNOUNCEMENT"),
                 }
                 requests.get('https://api.voicemonkey.io/trigger', params=params)
-        close(server)
-
-    print("Request sent.")
+                print("Request sent.")
+    close(server)
